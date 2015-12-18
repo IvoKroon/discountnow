c803683ad9f8100e8c6de4b03e9a58e8f43731f5
Ivo Kroon <ivokroon@MacBook-Pro-van-ivo.local>
2015-12-18 22:11:53 +0100

    added htaccess and made the types class bigger

--- includes/model/Types.php
+++ includes/model/Types.php
@@ -2,5 +2,18 @@
 
 class Types extends Model
 {
+    public function set(){
 
+    }
+    public function get($id){
+
+    }
+    public function all(){
+        try{
+            $query = "SELECT * FROM types";
+            $this->connection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        }catch (PDOException $e){
+            return $e->errorInfo;
+        }
+    }
 }
