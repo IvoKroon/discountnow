9f571e586094339d4446d308780e9a3e03e75ce1
Ivo Kroon <ivokroon@145-24-243-72.wlan.hro.nl>
2016-01-05 11:35:32 +0100

    Start with company show page

---
+++ includes/model/Company.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ivokroon
+ * Date: 05/01/16
+ * Time: 11:32
+ */
+class Company extends Model
+{
+
+
+}
--- includes/views/bedrijven.php
+++ includes/views/bedrijven.php
@@ -1,6 +1,2 @@
-<h1>bedrijven</h1>
-<div class="row">
-    <div class="col-md-3">test</div>
-    <div class="col-md-3">test1</div>
-    <div class="col-md-3">test3</div>
-    <div class="col-md-3">test4</div>
+
+<h1>Bedrijven</h1>
